d96696146ec1a288b4874a23087816811f555f08
Cod-lvr <79340509+cod-lvr@users.noreply.github.com>
2021-11-29 17:15:34 +0200

    add a mobile layout design

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,6 @@
+let i = 0;
+const colors = ["FF8C66", "56C2E6", "FF5C7C", "4ACF81", "7536D3", "F1C65B"];
+
 const cardContent = document.getElementById("card-content").content;
 const cardList = document.querySelector(".cards-container");
 
@@ -6,6 +9,13 @@ const weeklyBtn = document.querySelector(".weekly-btn");
 const monthlyBtn = document.querySelector(".monthly-btn");
 
 async function fetchCardData(timeFrame) {
+  if (cardList.querySelector("li")) {
+    cardList.querySelectorAll("li").forEach((card) => {
+      card.style.display = "none";
+    });
+    i = 0;
+  }
+
   const request = await fetch("./scripts/data.json");
   if (!request.ok) {
     throw new Error("faild to fetch data");
@@ -22,12 +32,14 @@ function createCard(card, timeFrame) {
   let prev = card.timeframes[timeFrame].previous;
 
   const newCard = cardContent.cloneNode(true);
+  newCard.querySelector("li").id = "active";
   newCard.querySelector("img").src = card.imgsrc;
   newCard.querySelector("#title").textContent = card.title;
-  newCard.querySelector("#current").textContent = crr;
-  newCard.querySelector("#previous").textContent = prev;
-
+  newCard.querySelector("#current").textContent = `${crr}hrs`;
+  newCard.querySelector("#previous").textContent = `Last Week- ${prev}hrs `;
+  newCard.querySelector("li").style["background-color"] = `#${colors[i]}`;
   cardList.appendChild(newCard);
+  i++;
 }
 
 dailyBtn.addEventListener("click", () => {
